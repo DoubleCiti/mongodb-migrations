04ce0df6c203e48b2d88a8adcceb121294949d10
David Xie <david.scriptfan@gmail.com>
2016-03-20 14:49:11 +0800

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
 from setuptools import setup, find_packages
 
-# get requirements
-install_requires = []
-with open('requirements.txt') as f:
-    for line in f.read().splitlines():
-        install_requires.append(line)
-
 setup(
     name='mongodb-migrations',
-    version='0.1.0',
+    version='0.1.1',
+    description='A database migration tool for MongoDB',
     long_description=__doc__,
-    url='https://github.com/DoubleCiti/daimaduan.com',
+    url='https://github.com/DoubleCiti/mongodb-migrations',
     author='David Xie',
     author_email='david.xie@me.com',
+    license='GPLv3',
     packages=find_packages(),
+    platforms='any',
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
-    entry_points = {
-        'console_scripts': ['mongo-migrate=mongodb_migrations.cli:main'],
-    }
+    install_requires=[
+        'pymongo'
+    ],
+    entry_points={
+        'console_scripts': ['mongodb-migrate=mongodb_migrations.cli:main'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
