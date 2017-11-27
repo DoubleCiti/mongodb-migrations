535e187a282d717f48af7c1dff70bbf38be22b71
jpchavat <jpchavat@expand.com.uy>
2017-11-27 16:19:15 -0300

    PEP8: Maximum line length.

--- mongodb_migrations/cli.py
+++ mongodb_migrations/cli.py
@@ -13,6 +13,8 @@ import pymongo
 #     create a file in custom folder
 # upgrade:
 #     run migration files from custom folder
+
+
 class MigrationManager(object):
     config = None
     db = None
@@ -23,7 +25,9 @@ class MigrationManager(object):
         self.config = Configuration()
 
     def run(self):
-        self.db = self._get_mongo_database(self.config.mongo_host, self.config.mongo_port, self.config.mongo_database)
+        self.db = self._get_mongo_database(
+            self.config.mongo_host, self.config.mongo_port,
+            self.config.mongo_database)
         files = os.listdir(self.config.mongo_migrations_path)
         for file in files:
             result = re.match('^(\d+)[_a-z]*\.py$', file)
@@ -31,12 +35,14 @@ class MigrationManager(object):
                 self.migrations[result.group(1)] = file[:-3]
 
         database_migrations = self._get_migration_names()
-        self.database_migration_names = [migration['migration_datetime'] for migration in database_migrations]
+        self.database_migration_names = [migration['migration_datetime']
+                                         for migration in database_migrations]
         if set(self.database_migration_names) - set(self.migrations.keys()):
             print("migrations doesn't match")
             sys.exit(1)
         if self.database_migration_names:
-            print("Found previous migrations, last migration is version: %s" % self.database_migration_names[0])
+            print("Found previous migrations, last migration is version: %s" %
+                  self.database_migration_names[0])
         else:
             print("No previous migrations found")
 
@@ -45,27 +51,34 @@ class MigrationManager(object):
 
     def _domigrate(self):
         for migration_datetime in sorted(self.migrations.keys()):
-            if not self.database_migration_names or migration_datetime > self.database_migration_names[0]:
-                print("Trying to migrate version: %s" % self.migrations[migration_datetime])
+            if not self.database_migration_names or \
+                    migration_datetime > self.database_migration_names[0]:
+                print("Trying to migrate version: %s" %
+                      self.migrations[migration_datetime])
                 try:
                     module = __import__(self.migrations[migration_datetime])
-                    migration_object = module.Migration(host=self.config.mongo_host,
-                                                        port=self.config.mongo_port,
-                                                        database=self.config.mongo_database)
+                    migration_object = module.Migration(
+                        host=self.config.mongo_host,
+                        port=self.config.mongo_port,
+                        database=self.config.mongo_database)
                     migration_object.upgrade()
                 except Exception as e:
-                    print("Failed to migrate version: %s" % self.migrations[migration_datetime])
+                    print("Failed to migrate version: %s" %
+                          self.migrations[migration_datetime])
                     print("%s" % e.message)
                     sys.exit(1)
-                print("Succeed to migrate version: %s" % self.migrations[migration_datetime])
+                print("Succeed to migrate version: %s" %
+                      self.migrations[migration_datetime])
                 self._create_migration(migration_datetime)
 
     def _get_migration_names(self):
-        return self.db.database_migrations.find().sort('migration_datetime', pymongo.DESCENDING)
+        return self.db.database_migrations.find().sort(
+            'migration_datetime', pymongo.DESCENDING)
 
     def _create_migration(self, migration_datetime):
-        self.db.database_migrations.save({'migration_datetime': migration_datetime,
-                                     'created_at': datetime.now()})
+        self.db.database_migrations.save(
+            {'migration_datetime': migration_datetime,
+             'created_at': datetime.now()})
 
     def _get_mongo_database(self, host, port, database):
         client = pymongo.MongoClient(host=host, port=port)
--- mongodb_migrations/config.py
+++ mongodb_migrations/config.py
@@ -18,16 +18,22 @@ class Configuration(object):
             raise Exception("No database name is provided")
 
     def _from_console(self):
-        self.arg_parser = argparse.ArgumentParser(description="Mongodb migration parser")
+        self.arg_parser = argparse.ArgumentParser(
+            description="Mongodb migration parser")
 
-        self.arg_parser.add_argument('--host', metavar='H', default=self.mongo_host,
-                            help="host of MongoDB")
-        self.arg_parser.add_argument('--port', type=int, metavar='p', default=self.mongo_port,
-                            help="port of MongoDB")
-        self.arg_parser.add_argument('--database', metavar='d',
-                            help="database of MongoDB", required=(self.mongo_database==None), default=self.mongo_database)
-        self.arg_parser.add_argument('--migrations', default=self.mongo_migrations_path,
-                            help="directory of migration files")
+        self.arg_parser.add_argument(
+            '--host', metavar='H', default=self.mongo_host,
+            help="host of MongoDB")
+        self.arg_parser.add_argument(
+            '--port', type=int, metavar='p', default=self.mongo_port,
+            help="port of MongoDB")
+        self.arg_parser.add_argument(
+            '--database', metavar='d', help="database of MongoDB",
+            required=(self.mongo_database is None),
+            default=self.mongo_database)
+        self.arg_parser.add_argument(
+            '--migrations', default=self.mongo_migrations_path,
+            help="directory of migration files")
 
         args = self.arg_parser.parse_args()
 
@@ -37,7 +43,10 @@ class Configuration(object):
         self.mongo_migrations_path = args.migrations
 
     def _from_ini(self):
-        self.ini_parser = ConfigParser(defaults={'host': self.mongo_host, 'port': self.mongo_port, 'migrations': self.mongo_migrations_path, 'database': self.mongo_database})
+        self.ini_parser = ConfigParser(
+            defaults={'host': self.mongo_host, 'port': self.mongo_port,
+                      'migrations': self.mongo_migrations_path,
+                      'database': self.mongo_database})
 
         try:
             fp = open(self.config_file)
@@ -47,9 +56,12 @@ class Configuration(object):
             with fp:
                 self.ini_parser.readfp(fp)
                 if not self.ini_parser.sections():
-                    raise Exception("Cannot find %s or it doesn't have sections." % self.config_file)
+                    raise Exception(
+                        "Cannot find %s or it doesn't have sections." %
+                        self.config_file)
 
                 self.mongo_host = self.ini_parser.get('mongo', 'host')
                 self.mongo_port = self.ini_parser.getint('mongo', 'port')
                 self.mongo_database = self.ini_parser.get('mongo', 'database')
-                self.mongo_migrations_path = self.ini_parser.get('mongo', 'migrations')
+                self.mongo_migrations_path = \
+                    self.ini_parser.get('mongo', 'migrations')
