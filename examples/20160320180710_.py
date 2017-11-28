098fa24f3aafeae8bf292c2ae00c45e997a7ef3e
jpchavat <jpchavat@expand.com.uy>
2017-11-28 14:55:47 -0300

    Adds class parameters previous_version and current_version to Migration (used when label type is hash)

--- examples/20160320180710_.py
+++ examples/20160320180710_.py
@@ -2,6 +2,10 @@ from mongodb_migrations.base import BaseMigration
 
 
 class Migration(BaseMigration):
+
+    current_version = '20160320180710'
+    previous_version = None
+
     def upgrade(self):
         self.db.test_collection.save({"new_column": "value"})
 
--- examples/20171017000000_mytest.py
+++ examples/20171017000000_mytest.py
@@ -2,6 +2,10 @@ from mongodb_migrations.base import BaseMigration
 
 
 class Migration(BaseMigration):
+
+    current_version = '20171017000000'
+    previous_version = '20160320180710'
+
     def upgrade(self):
         for item in self.db.test_collection.find():
             item['new_column1'] = "fake_value"
