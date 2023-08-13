08320dd91818d660e17c0fd1f53e6504fb936e4b
Darth Joker <darthjoker@outlook.ie>
2023-08-13 21:19:30 +0100

    Bump version to v1.3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_read_me():
 
 setup(
     name='mongodb-migrations',
-    version='1.2.1',
+    version='1.3.0',
     description=DESCRIPTION,
     long_description=get_read_me(),
     long_description_content_type="text/markdown",
